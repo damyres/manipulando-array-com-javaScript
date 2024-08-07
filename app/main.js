e9245d4cc562d2854129580fc1bf6a72faade8c7
Damyres <damyresmaciell@gmail.com>
2024-08-07 19:00:09 -0300

    Criar função para exibir os livros em tela com a descrição conforme é exibido no JSON

--- app/main.js
+++ app/main.js
@@ -3,10 +3,45 @@ const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json
 
 getBuscaLivrosDaAPI()
 
+const elementoParaInserirLivros = document.getElementById('livros')
+
 async function getBuscaLivrosDaAPI() {
 
     const res = await fetch(endpointAPI)
     livros = await res.json()
     console.table(livros)
-    
+    exibirOsLivrosNaTela(livros)
+
 }
+// praticando array com forEach
+// let frutas = ['maca', 'banana', 'abacaxi']
+
+// // for(let i=0; i<frutas.length; i++){
+// //     console.log(frutas[i])
+
+// // }
+
+// frutas.forEach((fruta, index) => {
+//     console.table(fruta, index)
+// })
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+     <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>     
+    `
+
+    });
+
+}
+
