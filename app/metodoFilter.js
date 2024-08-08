62ea985e706000d82659c44378ef8f81c2ecbf31
Damyres <damyresmaciell@gmail.com>
2024-08-08 20:02:14 -0300

    Listar livro de acordo com a categoria

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,8 +1,13 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
+// const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
 
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros)
+// btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros)
+
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros(){
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'back-end')
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
 }
