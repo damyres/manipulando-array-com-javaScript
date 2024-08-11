79fdbcec6e6106a2ec7742868f7e472450795646
Damyres <damyresmaciell@gmail.com>
2024-08-11 20:48:15 -0300

    Criar função para calcular o valor total dos livros disponíveis utilizando o método reduce

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -13,7 +13,8 @@ function filtrarLivros() {
         filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
         exibirOsLivrosNaTela(livrosFiltrados)
     if (categoria == 'disponivel') {
-        exibirValorTotalDosLivrosDisponiveisNaTela()
+        const valorTotal = calcularTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
     }
 }
 function filtrarPorCategoria(categoria) {
@@ -24,10 +25,11 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0)
 }
 
-function exibirValorTotalDosLivrosDisponiveisNaTela() {
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
     elementoComValorTotalDeLivroDisponioveis.innerHTML = `
     <div class="livros__disponiveis">
-      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>    
     `
+    console
 }
